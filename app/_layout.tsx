fa68885a39be06c46fdbbbb2e5795792ff76d4c0
lima8002 <57651553+lima8002@users.noreply.github.com>
2024-11-30 21:23:07 +0000

    Add splash screen configuration and implement custom layout with fonts. Update main index screen for a simple login app interface, including a logo and engagement button. Remove unused font and add new font files.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    outfit: require("../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+      <StatusBar style="dark" />
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,81 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
 
-export default function Index() {
+const index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.imageTitle}
+          source={require("../assets/images/sl-logo-circle.png")}
+        />
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text style={styles.bodyTitle}>Simple Login App</Text>
+        <Text style={styles.bodySubtitle}>
+          This is a simple login application built with React Native and
+          Firebase Authentication. It provides basic user authentication
+          functionality including sign up, login, logout and email verification
+          features.
+        </Text>
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btnStyle}>
+          <Text style={styles.btnText}>Let's get started</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: "30%",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  imageContainer: {
+    width: 320,
+    height: 320,
+    borderRadius: 90,
+    overflow: "hidden",
+  },
+  imageTitle: {
+    resizeMode: "cover",
+    width: "98%",
+    height: "100%",
+  },
+  bodyContainer: {
+    paddingHorizontal: 25,
+  },
+  bodyTitle: {
+    fontSize: 28,
+    fontFamily: "outfit-medium",
+    textAlign: "center",
+    paddingTop: 20,
+  },
+  bodySubtitle: {
+    paddingTop: 30,
+    fontSize: 20,
+    fontFamily: "outfit",
+    textAlign: "justify",
+  },
+  btnContainer: {
+    paddingTop: 30,
+    width: "60%",
+    alignSelf: "center",
+  },
+  btnStyle: {
+    borderWidth: 1,
+    borderRadius: 25,
+  },
+  btnText: {
+    textAlign: "center",
+    fontFamily: "outfit-medium",
+    fontSize: 16,
+    paddingVertical: 12,
+  },
+});
